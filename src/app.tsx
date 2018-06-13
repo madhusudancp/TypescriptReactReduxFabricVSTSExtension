bbcfb3a964dac68bad073a270b37a2af249d224b
C P <macp@paypal.com>
2018-06-13 22:06:41 +0530

    Working REDUX integration

--- src/app.tsx
+++ src/app.tsx
@@ -3,14 +3,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { ValueStreams } from "./containers/valueStreams";
-
+import { AppTodo } from "./components/AppTodo";
+import { Provider } from 'react-redux'
+import store from './store'
 import "./styles/main.scss";
 
 export function init(containerId: string) {
     ReactDOM.render((
+        <Provider store={store}>
         <div className="hub-view">
-            <ValueStreams />
+            <AppTodo />
         </div>
+        </Provider>
     ), document.getElementById(containerId));
 }
---
+++ src/components/AppTodo.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import AddTodo from '../containers/AddTodo'
+import Todos from '../containers/Todos'
+
+export  class AppTodo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Todos</h1>
+        <AddTodo />
+        <Todos />
+      </div>
+    );
+  }
+}
+export default AppTodo;
